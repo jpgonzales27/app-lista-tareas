4cc4d002bf4a50646b2e11322cc8403ff5653185
Juan Pablo Gonzales <juampi7237@gmail.com>
2023-02-08 18:13:22 -0400

    formulario tareas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import FormularioTareas from "./componentes/FormularioTareas";
 import Header from "./componentes/Header";
 
 const App = () => {
+  const [tareas, setTareas] = useState([
+    {
+      id: 1,
+      texto: "Trabajar",
+      completada: false,
+    },
+    {
+      id: 2,
+      texto: "programar",
+      completada: false,
+    },
+  ]);
+
+  console.log(tareas);
   return (
     <div className="contenedor">
       <Header />
+      <FormularioTareas tareas={tareas} setTareas={setTareas} />
     </div>
   );
 };
